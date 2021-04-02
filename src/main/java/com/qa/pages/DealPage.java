78b1784ee521fc2f8733f1abe27c347e5e69b896
kishan <patelkishan0715@gmail.com>
2021-04-02 11:08:20 -0400

    I have updated my login page and addedd new deal page

---
+++ src/main/java/com/qa/pages/DealPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class DealPage {
+
+	public void dealPagedeatails() {
+		System.out.println("this is my deal page");
+	}
+}
--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 
 	public void page1() {
 		System.out.println("this is my login page");
+		System.out.println("i am adding something on login page after first push");
 	}
 }
